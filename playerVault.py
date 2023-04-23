788e22e16f73fefefc7ba9768a44b72be4908a16
Salsasp <dakotagee99@gmail.com>
2023-04-23 17:02:30 -0500

    forgot to add this

---
+++ playerVault.py
@@ -0,0 +1,10 @@
+import pydest
+import asyncio
+#TODO: add code to dehash all weapons in player vault, add them to a list, and use some sort of data structure(undetermined) to sort weapons based on their different attributes
+class Vault: 
+
+    def __init__(self, playerVaultData):
+        pass #change later
+
+    def decodeWeaponHashes(weaponList):
+        pass #change later
