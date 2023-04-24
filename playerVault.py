0ccefef88c06b408d4f696634865196b1857d8a2
Dakota Gee <dakotagee99@gmail.com>
2023-04-24 13:47:02 -0500

    more changes

--- playerVault.py
+++ playerVault.py
@@ -4,7 +4,10 @@ import asyncio
 class Vault: 
     def __init__(self, playerVaultData):
         self.itemData = playerVaultData
-        #self.weapons = self.decodeWeaponHashes(self.itemData)
+        self.weaponTags = self.generateWeaponTags(playerVaultData)
 
     def decodeWeaponHashes(weaponList):
         pass #change later
+
+    def generateWeaponTags(weapons):
+        pass #TODO: create a map of weapons with keys corresponding to weapon atributes (eg. energy type, weapon type, etc.)
